a679ba871de9bb73eca0ec7424c0e545b74852c5
heartbreathing <li.wang@hyperisland.se>
2023-11-13 09:40:01 +0100

    modified some style

--- React/todo-list/src/Task.js
+++ React/todo-list/src/Task.js
@@ -8,22 +8,22 @@ export const Task = (props) => {
 
 return (
   <div className="bg-white p-4 mb-2 shadow-md flex flex-col sm:flex-row justify-between items-center">
-    <div className="flex-grow sm:flex-grow-0">
+    <div className="flex items-center">
       <p className={`text-lg ${props.isCompleted ? 'line-through text-gray-500' : 'text-black'}`}>
         {props.taskName}
       </p>
-      <p className="text-sm text-gray-500">{props.dueDate}</p>
+      <p className="text-sm text-gray-500 ml-2">{props.dueDate}</p>
     </div>
-    <div className="mt-2 sm:mt-0">
+    <div className="mt-2 sm:mt-0 flex items-center">
       <button
         onClick={handleCompleteClick}
-        className={`px-4 py-2 ${props.isCompleted ? 'bg-green-400' : 'bg-blue-500'} text-white rounded`}
+        className={`px-4 py-2 ${props.isCompleted ? 'bg-green-400' : 'bg-blue-500'} text-white rounded ml-2`}
       >
         {props.buttonText}
       </button>
       <button
         onClick={() => props.deleteTodo(props.id)}
-        className="bg-red-500 hover:bg-red-600 text-white p-2 rounded ml-2"
+        className="bg-red-500 hover-bg-red-600 text-white p-2 rounded ml-2"
       >
         X
       </button>
